66d9f438402c6bb140baa32ef48c3dbecf8ad73a
Florian Werner <flwer@hrz.tu-chemnitz.de>
2021-04-08 23:51:30 +0200

    add minimal translation for Group -> Project (en ,de)

--- ckanext/theme_tuc/plugin.py
+++ ckanext/theme_tuc/plugin.py
@@ -1,14 +1,14 @@
-import ckan.plugins as plugins
-import ckan.plugins.toolkit as toolkit
+from ckan import plugins
+from ckan.plugins import toolkit
+from ckan.lib.plugins import DefaultTranslation
 
 
-class ThemeTucPlugin(plugins.SingletonPlugin):
+class ThemeTucPlugin(plugins.SingletonPlugin, DefaultTranslation):
     plugins.implements(plugins.IConfigurer)
 
     # IConfigurer
 
-    def update_config(self, config_):
-        toolkit.add_template_directory(config_, 'templates')
-        toolkit.add_public_directory(config_, 'public')
-        toolkit.add_resource('fanstatic',
-            'theme_tuc')
+    def update_config(self, config):
+        toolkit.add_template_directory(config, 'templates')
+        toolkit.add_public_directory(config, 'public')
+        toolkit.add_resource('fanstatic', 'theme_tuc')
